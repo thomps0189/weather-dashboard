2e14080b4cb7f4794353a234c7e4c733b84f2f75
thomps0189 <angelathompson2011@gmail.com>
2021-11-19 15:26:04 -0600

    initial js

---
+++ assets/javascript/index.js
@@ -0,0 +1,9 @@
+var locationFormEl = document.querySelector("#location")
+var locationInputEl = document.querySelector("#location-input");
+
+
+var getWeatherData = function() {
+    console.log("weather")
+}
+
+getWeatherData();
