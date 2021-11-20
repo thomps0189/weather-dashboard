807b8d3971b648111aad1e209fd48a6b865e3823
thomps0189 <angelathompson2011@gmail.com>
2021-11-20 13:56:11 -0600

    fetch weather data

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,24 +1,32 @@
 var locationFormEl = document.querySelector("#location")
 var locationInputEl = document.querySelector("#location-input");
+var city = document.getElementById("location-input").value;
 
-
-var getWeatherData = function(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=ba3af0c57267c9b751ea6ef5a926e30c"
+let getWeatherData = {
+    search: function() {
+        this.fetchWeather(document.querySelector(".city-state").value);
+    },
     
-     var response = fetch("https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=ba3af0c57267c9b751ea6ef5a926e30c"
-    ).then(function(response) {
-        response.json().then(function(data) {
-          console.log(data)  
-        })
-        
-    })
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ba3af0c57267c9b751ea6ef5a926e30c")
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
 
-    console.log("outside")
-    
-    // .then((response) => response.json())
-    // .then((data) => console.log(data));   
-    // }
-    
-}
+    displayWeather: function(data) {
+        const { name } = data;
+        const { temp } = data.main;
+        const { speed } = data.wind;
+        const { humidity } = data.main;
+        console.log(name, temp, speed, humidity)
+        document.querySelector(".city-name-submitted").innerText = "Weather in " + name;
+        document.querySelector(".temp").innerText = "Temperature: " + temp + "F";
+        document.querySelector(".speed").innerText = "Wind Speed " + speed + " mph";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    },
 
-getWeatherData();
+    
+};
+
+
+document.querySelector(".btn").addEventListener("click", getWeatherData.search());
