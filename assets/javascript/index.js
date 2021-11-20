f0ae3b19807aabb4c4a3b845377fa91f945e63c3
thomps0189 <angelathompson2011@gmail.com>
2021-11-19 18:18:06 -0600

    api url

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,8 +2,23 @@ var locationFormEl = document.querySelector("#location")
 var locationInputEl = document.querySelector("#location-input");
 
 
-var getWeatherData = function() {
-    console.log("weather")
+var getWeatherData = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=ba3af0c57267c9b751ea6ef5a926e30c"
+    
+     var response = fetch("https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=ba3af0c57267c9b751ea6ef5a926e30c"
+    ).then(function(response) {
+        response.json().then(function(data) {
+          console.log(data)  
+        })
+        
+    })
+
+    console.log("outside")
+    
+    // .then((response) => response.json())
+    // .then((data) => console.log(data));   
+    // }
+    
 }
 
 getWeatherData();
